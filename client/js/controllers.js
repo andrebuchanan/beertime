639bfb6bc3f1ffcc365fc354725d9340c7eae313
rogerano <rogerano@gmail.com>
2013-07-02 16:04:21 +1000

    fixed bugs, stuff works now

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -10,6 +10,8 @@ angular.module('arbapp.controllers', []).
   controller("appCtrl", function($scope, $timeout, Att, events, topic, $window)
   {
     $scope.attendees = Att.query();
+    $scope.dels = {};
+    $scope.showAttPanel = true;
 
     $scope.update = function(att)
     {
@@ -18,11 +20,24 @@ angular.module('arbapp.controllers', []).
 
     $scope.add = function(att)
     {
-      Att.save(att, function(newAtt)
+      Att.save(att);
+    };
+
+    $scope.undo = function(delguy)
+    {
+      Att.get(delguy, function(oldguy)
       {
-        $scope.attendees.push(newAtt)        ;
+        oldguy.deleted = false;
+        oldguy.$update();
       });
-    }
+    };
+
+    $scope.del = function(delme)
+    {
+      $scope.dels[delme.name] = delme;
+      Att.delete(delme);
+      $scope.delMessage = "You've deleted " + delme.name + ". Undo?";
+    };
 
     var eventHorizon = new Date("Jul 5, 2013 16:30:00");
     $scope.beerDate = eventHorizon;
@@ -39,15 +54,36 @@ angular.module('arbapp.controllers', []).
     };
     $scope.go();
 
+    $scope.updateList = function(attToFind)
+    {
+      var added = false;
+      angular.forEach($scope.attendees, function(att)
+      {
+        if (att.name === attToFind.name)
+        {
+          added = true;
+          $scope.attendees[$scope.attendees.indexOf(att)] = attToFind;
+        }
+      });
+      if (!added)
+      {
+        $scope.attendees.push(attToFind);
+      }
+    };
+
     topic.subscribe("evt/rfs", function()
     {
       $window.location.reload();
     });
 
+    topic.subscribe("evt/del", function(message)
+    {
+      $scope.updateList(message.att);
+    });
+
     topic.subscribe("evt/att", function(message)
     {
-      console.log(message, $scope.attendees);
-      Att.cache(message.att);
+      $scope.updateList(message.att);
     });
   });
 })();
